665d6727190f43cd77f58227079c2cfe179dc5a1
felix-blueinno <94157332+felix-blueinno@users.noreply.github.com>
2022-05-04 17:36:09 +0800

    installed flask and test run homepage

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from blockchain import Blockchain
+from flask import Flask
+
+app = Flask("app name")
+
+@app.route("/")
+def homepage():
+    return "Hello there~~~"
 
 chain = Blockchain()
 
@@ -10,3 +17,4 @@ chain.add_transaction("tx2")
 chain.mine()
 chain.show()
 
+app.run(host="0.0.0.0", port=8000)
