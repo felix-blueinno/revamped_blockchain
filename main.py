b41d01df484cf641bbe8a5bf3416a51bca0d0d4f
felix-blueinno <94157332+felix-blueinno@users.noreply.github.com>
2022-05-04 17:44:41 +0800

    able to accept POST request to add new tx

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from flask import Flask, request
 from blockchain import Blockchain
 from flask import Flask
 import json
 
 app = Flask("app name")
+chain = Blockchain()
 
 
 @app.route("/")
@@ -19,10 +21,26 @@ def get_chain():
 @app.route('/unmined_blocks', methods=['GET'])
 def get_unmined_blocks():
     chain_dict = {"unmined blocks": chain.unmined_chain}
+
     return json.dumps(chain_dict, default=lambda obj: obj.__dict__)
 
 
-chain = Blockchain()
+@app.route('/new_transaction', methods=['POST'])
+def new_transaction():
+    tx_data = request.get_json()
+    required_fields = ['sender', 'amount', 'recipient']
+    for field in required_fields:
+        if not tx_data.get(field):
+            return {"result": "Invalid transaction data"}, 400
+
+    sender = tx_data['sender']
+    amount = tx_data['amount']
+    recipient = tx_data['recipient']
+
+    transaction = f'{sender}, {amount}, {recipient}'
+    chain.add_transaction(transaction)
+    return {"result": "Transaction added successfully"}, 200
+
 
 chain.add_transaction("tx1")
 chain.mine()
