19d04e916f1b7e206713d4da5d047aed4ef835cf
felix-blueinno <94157332+felix-blueinno@users.noreply.github.com>
2022-05-04 17:39:53 +0800

    demo & exercise of GET endpoints

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from blockchain import Blockchain
 from flask import Flask
+import json
 
 app = Flask("app name")
 
+
 @app.route("/")
 def homepage():
-    return "Hello there~~~"
+    return f'we have {len(chain.chain)} blocks now.'
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_dict = {"length": len(chain.chain), "chain": chain.chain}
+    return json.dumps(chain_dict, default=lambda obj: obj.__dict__)
+
+
+@app.route('/unmined_blocks', methods=['GET'])
+def get_unmined_blocks():
+    chain_dict = {"unmined blocks": chain.unmined_chain}
+    return json.dumps(chain_dict, default=lambda obj: obj.__dict__)
+
 
 chain = Blockchain()
 
