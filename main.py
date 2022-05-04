9aa3d65530eb63a5397d3908afe50040b2fea314
felix-blueinno <94157332+felix-blueinno@users.noreply.github.com>
2022-05-04 18:22:14 +0800

    removed excessive instantiation in main.py

--- main.py
+++ main.py
@@ -5,9 +5,5 @@ from flask_server import FlaskServer
 chain = Blockchain()
 users: List[dict] = []
 
-
-chain = Blockchain()
-users = []
-
 server = FlaskServer(chain=chain, users=users)
 server.run()
