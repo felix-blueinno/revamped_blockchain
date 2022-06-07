d4692419512d2207ca3450dc4d5edcdad8d51b34
felix-blueinno <94157332+felix-blueinno@users.noreply.github.com>
2022-06-07 11:52:31 +0800

    modified: peer registration in threads

--- nodes.py
+++ nodes.py
@@ -1,6 +1,7 @@
 import os
 from constants import NODES_FILENAME, MASTER_NODE
 import requests
+import _thread
 
 
 class Nodes:
@@ -25,33 +26,47 @@ class Nodes:
                 f.write(peer + ',')
 
     def knock_peers(self):
-        new_peers = set()
+
         for peer in self.peers:
             try:
-                response = requests.get(f'{peer}/peers', timeout=5)
+                print('knocking: ', peer)
+                response = requests.get(f'{peer}peers', timeout=5)
                 if response.status_code == 200:
+
                     peers = response.json()['peers']
+                    new_peer = set()
+
                     for peer in peers:
-                        new_peers.add(peer)
+                        new_peer.add(peer)
+                    self.peers = self.peers.union(new_peer)
                     break
 
             except Exception as e:
-                print("Exception occurs: ", e)
+                print('+---------------------------------------------')
+                print('Error connecting to: ', peer, '\nError: ', e)
+                print('+---------------------------------------------')
                 self.failed_connect_peers.add(peer)
 
-        for peer in new_peers:
-            self.add_node(peer)
+        self.peers = self.peers - self.failed_connect_peers
+        print('+---------------------------------------------')
+        print('self.failed_connect_peers: ', self.failed_connect_peers)
+        print('+---------------------------------------------')
+        print('self.peers: ', self.peers)
+        print('+---------------------------------------------')
 
-        for peer in self.peers:
-            print("Knocking door of: ", peer)
+        def knock_peer(address: str):
             try:
-                response = requests.post(peer + 'register_node',
+                response = requests.post(address + 'register_node',
                                          json={"node_address": self.root_url},
                                          timeout=5)
 
                 if response.status_code != 200:
-                    self.failed_connect_peers.add(peer)
+                    self.failed_connect_peers.add(address)
 
             except Exception as e:
                 print(e)
-                self.failed_connect_peers.add(peer)
+                self.failed_connect_peers.add(address)
+
+        for peer in self.peers:
+            print("Registering with: ", peer)
+            _thread.start_new_thread(knock_peer, (peer, ))
